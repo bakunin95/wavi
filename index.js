f2d83a30c36d505ead4c1d8d303ddbf369744462
bakunin95 <jugle66@hotmail.com>
2014-11-30 12:28:42 -0500

    Added CSS selectors in CSS classes

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var relex = require('relex');
 var graphvizClass = require('./lib/deps/graphvizClass');
 
-exports.generateGraph = function(type,website_path,result_file,include_node_modules,callback) {
+
+
+exports.generateGraph = function(type,website_path,result_file,include_node_modules,cbGenGraph) {
 
 	relex.extract(website_path,include_node_modules,function(err,report){	
 		errorHandler(err);	
@@ -9,9 +11,36 @@ exports.generateGraph = function(type,website_path,result_file,include_node_modu
 		if(type == "jpg" || type == "png" || type == "svg" || type == "dot" || type == "pdf"){
 			graphvizClass.generateGraph(JSON.stringify(report),type,result_file,false,function(err){
 				errorHandler(err)
+				cbGenGraph(err);
 			}); 
 		}
+		else{
+			cbGenGraph(err,"");
+		}
 	});	
+
+	function errorHandler(err){
+		if(err){
+			console.log("Error (main.js):",err);
+		}	
+	}
+
+};
+exports.generateGraphFromJSON = function(type,report,result_file,cbGenGraphtwo) {
+
+	if(type == "jpg" || type == "png" || type == "svg" || type == "dot" || type == "pdf"){
+
+		graphvizClass.generateGraph(JSON.stringify(report),type,result_file,true,function(err){
+			
+			errorHandler(err);
+			
+
+			cbGenGraphtwo(err,"");
+		}); 
+	}
+	else{
+		cbGenGraph(err,"");
+	}
 
 	function errorHandler(err){
 		if(err){
